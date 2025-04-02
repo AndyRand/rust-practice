6a1af7762562a002c8ef4497adf1c3737cfdb79b
Andy Randrianirina <arandrianirina@noesio.fr>
2025-04-03 00:11:39 +0200

    exo 12 part 2

--- src/main.rs
+++ src/main.rs
@@ -7,7 +7,7 @@ fn main() {
     // let (query, file_path) = parse_config(&args);
     // let config = parse_config(&args);
     let config = Config::build(&args).unwrap_or_else(|err| {
-        println!("Problem parsing arguments: {err}");
+        eprintln!("Problem parsing arguments: {err}");
         process::exit(1);
     });
 
@@ -16,7 +16,7 @@ fn main() {
     println!("In file {}", config.file_path);
 
     if let Err(e) = rust_practice::run(config) {
-        println!("Application error: {e}");
+        eprintln!("Application error: {e}");
         process::exit(1);
     }
 }
